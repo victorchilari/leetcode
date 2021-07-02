3087a1cdaa69f2168086e6756f4750b425789cee
victorchilari <victor1chilari1@gmail.com>
2021-07-02 16:17:21 +0300

    Change icons from dousaurus' to leetcode's

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
 	baseUrl: '/leetcode/',
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
-	favicon: 'img/favicon.ico',
+	favicon: 'https://assets.leetcode.com/static_assets/public/icons/favicon-96x96.png',
 	organizationName: 'victorchilari', // Usually your GitHub org/user name.
 	projectName: 'leetcode', // Usually your repo name.
 	themeConfig: {
@@ -17,7 +17,7 @@ module.exports = {
 			title: 'Solutions for LeetCode Patterns',
 			logo: {
 				alt: 'My Site Logo',
-				src: 'img/logo.svg'
+				src: 'https://assets.leetcode.com/static_assets/public/icons/favicon-192x192.png'
 			},
 			items: [
 				{
