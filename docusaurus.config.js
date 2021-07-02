e067c31771a9e7560fe9258dfcdcf6bbd09a5c81
victorchilari <victor1chilari1@gmail.com>
2021-07-02 13:32:07 +0300

    fix of deploy with npm scripts

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,10 +66,10 @@ module.exports = {
 				{
 					title: 'More',
 					items: [
-						{
-							label: 'Blog',
-							to: '/blog'
-						},
+						// {
+						// 	label: 'Blog',
+						// 	to: '/blog'
+						// },
 						{
 							label: 'GitHub',
 							href: 'https://github.com/victorchilari/leetcode'
