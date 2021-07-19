a717f3fdb5bcda50e671743b402f65131569e1f7
victorchilari <victor1chilari1@gmail.com>
2021-07-19 18:11:23 +0300

    refactor/ Hide "edit content" #1
    Didn't found how to fix, after click - client is redirected on "/edit/master/website/docs/..."

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,16 +88,16 @@ module.exports = {
 		[
 			'@docusaurus/preset-classic',
 			{
-				docs: {
-					sidebarPath: require.resolve('./sidebars.js'),
-					// Please change this to your repo.
-					editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/'
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/'
-				},
+				// docs: {
+				// 	sidebarPath: require.resolve('./sidebars.js'),
+				// 	// Please change this to your repo.
+				// 	editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/'
+				// },
+				// blog: {
+				// 	showReadingTime: true,
+				// 	// Please change this to your repo.
+				// 	editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/'
+				// },
 				theme: {
 					customCss: require.resolve('./src/css/custom.css')
 				}
