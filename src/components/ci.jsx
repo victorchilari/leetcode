9a1868236ff0f14c67881fbeb5ec6be944421af9
victorchilari <victor1chilari1@gmail.com>
2021-08-05 14:20:31 +0300

    sketch/ try to copy UI element: textarea to input code

--- src/components/ci.jsx
+++ src/components/ci.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import styles from './ci.module.css';
+
+// import CodeInput from './codeinput.mdx';
 
 export default () => (
 	<>
 		<div className="container padding-top--md padding-bottom--lg">
-			<div class="alert alert--primary" role="alert">
-				<button aria-label="Close" class="clean-btn close" type="button">
+			<div className="alert alert--primary" role="alert">
+				<button aria-label="Close" className="clean-btn close" type="button">
 					<span aria-hidden="true">×</span>
 				</button>
 				This is a <strong>primary</strong> alert. You should probably pay
@@ -12,20 +15,36 @@ export default () => (
 			</div>
 		</div>
 		<div className="container padding-top--md padding-bottom--lg">
-			<div class="row">
-				<div class="col col--3">
-					<button aria-label="Close" class="clean-btn close" type="button">
+			<div className="row">
+				<div className="col col--3">
+					<button aria-label="Close" className="clean-btn close" type="button">
 						<span aria-hidden="true">×</span>
 					</button>
 					This is a <strong>primary</strong> alert. You should probably pay
 					attention to it.
 				</div>
-				<div class="col">
-					<button aria-label="Close" class="clean-btn close" type="button">
+				<div className="col">
+					<button aria-label="Close" className="clean-btn close" type="button">
 						<span aria-hidden="true">×</span>
 					</button>
 					This is a <strong>primary</strong> alert. You should probably pay
 					attention to it.
+				</div>
+			</div>
+			<div className={styles.playgroundContainer}>
+				<div className={styles.playgroundHeader}>Live Editor</div>
+				<div className={styles.playgroundEditor}>
+					<textarea
+						name="condition_input"
+						id="condition_input"
+						autocapitalize="off"
+						autocomplete="off"
+						autocorrect="off"
+						spellcheck="false"
+						data-gramm="false"
+					>
+						Live Editor
+					</textarea>
 				</div>
 			</div>
 		</div>
