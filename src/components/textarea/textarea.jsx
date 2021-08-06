77f10537fed286d697e40acb3aad6a39dad43d3d
victorchilari <victor1chilari1@gmail.com>
2021-08-06 11:39:13 +0300

    feature/ Textarea/ Improved spead of height calc by use another method of creation
    https://css-tricks.com/auto-growing-inputs-textareas/#other-ideas

--- src/components/textarea/textarea.jsx
+++ src/components/textarea/textarea.jsx
@@ -1,39 +1,22 @@
 import React, { useRef } from 'react';
 import styles from './textarea.module.css';
 
-const defSettings = {
-	minHeight: 20,
-	lineHeight: 20,
-	padding: 12,
-	border: 2
-};
-
 const Textarea = () => {
-	const textarea = useRef(null);
+	const parentNode = useRef(null);
 
-	// Dealing with Textarea Height
-	function calcHeight(value) {
-		let numberOfLineBreaks = (value.match(/\n/g) || []).length;
-		// min-height + lines x line-height + padding + border
-		let newHeight =
-			defSettings.minHeight +
-			numberOfLineBreaks * defSettings.lineHeight +
-			defSettings.padding +
-			defSettings.border;
-		return newHeight;
-	}
-
-	function recalcTextareaHeight() {
-		textarea.current.style.height = calcHeight(textarea.current.value) + 'px';
-	}
-
+	const handleTextarea = (value) => (parentNode.current.dataset.value = value);
 	return (
-		<textarea
-			ref={textarea}
-			onKeyUp={recalcTextareaHeight}
-			defaultValue=""
-			className={styles.textarea}
-		/>
+		<label
+			ref={parentNode}
+			className={`${styles.inputSizer} ${styles.stacked}`}
+		>
+			<textarea
+				onChange={(e) => handleTextarea(e.target.value)}
+				rows="1"
+				placeholder="hi"
+				defaultValue=""
+			/>
+		</label>
 	);
 };
 
